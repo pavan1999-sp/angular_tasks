4eee4c4cde0ac48412414c9a1794a115bbab00c4
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-12-15 10:07:38 +0530

    lokesh

---
+++ lokesh/examination-master/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { QuestionComponent } from './question/question.component'
+import { FormsModule } from '@angular/forms';
+import { ResultComponent } from './result/result.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuestionComponent,
+    ResultComponent,
+    HomeComponent,
+    NavbarComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
