9d977ec58b58dba0f4ba19ab45ba554e45f63e9c
sagilokeshvarma98 <sagilokeshvarma98@gmail.com>
2021-12-20 11:46:01 +0530

    20/12

--- lokesh/contactTask/src/app/components/no-contacts/no-contacts.component.ts
+++ lokesh/contactTask/src/app/components/no-contacts/no-contacts.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-no-contacts',
   templateUrl: './no-contacts.component.html',
   styleUrls: ['./no-contacts.component.css']
 })
-export class NoContactsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NoContactsComponent {
 }
